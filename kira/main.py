d1ecdf347c64edad758e23df3ad7462157dbccaa
Tishir614 <abdrahmanovartur30@gmail.com>
2026-09-19 11:44:46 +0000

    Expose PUBG training navigation map

--- kira/main.py
+++ kira/main.py
@@ -390,6 +390,9 @@ async def pubg_map(): return pubg_training_map()
 @app.get("/pubg/training/map")
 async def pubg_map(): return pubg_training_map()
 
+@app.get("/pubg/training/map")
+async def pubg_training_map_state(): return pubg_training_map()
+
 @app.get("/pubg/training/weapons")
 async def pubg_training_weapons(): return pubg_weapon_knowledge()
 
