eae4f029c22c5a802781124d23d26a3f0a32dd8e
Tishir614 <abdrahmanovartur30@gmail.com>
2026-09-19 15:10:23 +0000

    Sync Android bridge version 1.4.0

--- android/app/src/main/java/ai/kira/studio/KiraBridge.kt
+++ android/app/src/main/java/ai/kira/studio/KiraBridge.kt
@@ -1,7 +1,7 @@
 package ai.kira.studio
 import android.webkit.JavascriptInterface
 class KiraBridge(private val activity:MainActivity){
- @JavascriptInterface fun appVersion()="1.3.0"
+ @JavascriptInterface fun appVersion()="1.4.0"
  @JavascriptInterface fun changeServer(){activity.runOnUiThread{activity.openServerDialog()}}
  @JavascriptInterface fun reload(){activity.runOnUiThread{activity.reloadStudio()}}
 }
